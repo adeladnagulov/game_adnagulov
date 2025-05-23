7f7406dce9d3bede5685f2ce78740b42d1d18cdd
Адель Аднагулов <adeladndjc@gmail.com>
2025-05-23 14:33:41 +0500

    добавил model

---
+++ игра/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace игра
+{
+    public class GameController
+    {
+        public GameController()
+        {
+            bg1 = new Background(0, 0);
+            bg2 = new Background(bg1.Width, 0);
+            player = new Player(77, 355);
+            peak = new ThePeak(250, 465);
+        }
+    }
+}
---
+++ игра/Models.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace игра
+{
+    internal class Player
+    {
+        public float X;
+        public float Y;
+        public int Size;
+        public Image PlayerImg;
+        public float GravityValue;
+        public float JumpSpeed;
+        public float FallSpeed;
+        public bool IsJumping;
+
+        public Player(int x, int y)
+        {
+            PlayerImg = new Bitmap("C:\\Users\\adela\\OneDrive\\Desktop\\работы\\С шарп\\игра\\игра\\player.png");
+            this.X = x;
+            this.Y = y;
+            Size = 60;
+            GravityValue = 0.1f;
+            IsJumping = false;
+            JumpSpeed = 0;
+            FallSpeed = 0;
+        }
+    }
+
+    internal class Background
+    {
+        public Image BackgroundImage;
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+
+        public Background(int x, int y)
+        {
+            BackgroundImage = Image.FromFile("C:\\Users\\adela\\OneDrive\\Desktop\\работы\\С шарп\\игра\\игра\\background.jpg");
+            this.X = x;
+            this.Y = y;
+            Width = 800;
+            Height = 600;
+        }
+    }
+
+    internal class ThePeak
+    {
+        public float X;
+        public float Y;
+        public int Size;
+        public Image PeakImg;
+
+        public ThePeak(int x, int y)
+        {
+            PeakImg = new Bitmap("C:\\Users\\adela\\OneDrive\\Desktop\\работы\\С шарп\\игра\\игра\\R.png");
+            this.X = x;
+            this.Y = y;
+            Size = 50;
+        }
+    }
+}
